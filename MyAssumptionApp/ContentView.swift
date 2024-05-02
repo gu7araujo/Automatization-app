f9d407d91c918ffd3b54afe55989661535a2e36b
Gustavo Araujo Santos <gustavo.gustavo-ext@ab-inbev.com>
2024-05-02 14:13:57 -0300

    Initial Commit

---
+++ MyAssumptionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyAssumptionApp
+//
+//  Created by Gustavo Araujo Santos on 02/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAssumptionApp/MyAssumptionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssumptionAppApp.swift
+//  MyAssumptionApp
+//
+//  Created by Gustavo Araujo Santos on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAssumptionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
